cfa779003c5109cff9a34013d2c3c3f7bb8adc48
Juerd Waalboer <juerd@tnx.nl>
2020-10-24 17:42:01 +0200

    struct initializer instead of constructor, make units configurable, cleanups
    (Should have been separate commits.)
    
    The main thing I dislike about C++ is that it lacks named parameters for
    functions and constructors. Heterogenous positional lists are a design flaw.
    Fortunately, there's the old key/value struct initialization. Unfortunately, it
    can't deal with elements that have a default (seriously? In many cases that's
    exactly why you want named items in the first place, so you can have a nice
    number of configurable things that can be left out...) so now there's a
    misleading "enabled = false". You win some, you lose some.

--- snuffel.ino
+++ snuffel.ino
@@ -13,11 +13,11 @@ using namespace std;
 #define Sprintf(f, ...) ({ char* s; asprintf(&s, f, __VA_ARGS__); String r = s; free(s); r; })
 
 const int interval = 5000;
-
 const int buttonpin  = 15;
 
 MQTTClient mqtt;
 String topic_prefix;
+bool add_units;
 
 void retain(String topic, String message) {
     Serial.printf("%s %s\n", topic.c_str(), message.c_str());
@@ -34,19 +34,11 @@ struct SnuffelSensor {
     FN      init;
     FN      request;
     LM      fetch;
-    SnuffelSensor(
-        String i, String n, String t, FN s, FN r, LM f
-    ) : id(i), description(n), topic_suffix(t), init(s), request(r), fetch(f) {}
 
     void publish(list<pair<const char*, String>> mapping, String value, String unit) {
         String topic = topic_prefix + topic_suffix;
-        for (auto i : mapping) {
-            topic.replace(i.first, i.second);
-        }
-        if (true) {
-            value += " " + unit;
-        }
-        retain(topic, value);
+        for (auto i : mapping) topic.replace(i.first, i.second);
+        retain(topic, add_units ? (value + " " + unit) : value);
     }
     void publish(String value, String unit) {
         publish({ }, value, unit);
@@ -60,18 +52,19 @@ void setup_sensors() {
         static OneWire ds(4);
         static DallasTemperature sensors(&ds);
 
-        snuffels.push_back(SnuffelSensor {
-            "DS18B20",
-            "temperature sensor(s)",
-            "temperature/{index}",
-            []() { // init
+        struct SnuffelSensor s = {
+            enabled: false,  // enabled by default via WiFiSettings
+            id: "DS18B20",
+            description: "temperature sensor(s)",
+            topic_suffix: "temperature/{index}",
+            init: []() {
                 sensors.begin();
                 sensors.setWaitForConversion(false);
             },
-            []() { // request
+            request: []() {
                 sensors.requestTemperatures();
             },
-            [](SnuffelSensor& self) { // fetch
+            fetch: [](SnuffelSensor& self) {
                 unsigned int timeout = millis() + 500;
                 while (millis() < timeout && !sensors.isConversionComplete()) { }
 
@@ -80,31 +73,34 @@ void setup_sensors() {
                     if (C == DEVICE_DISCONNECTED_C) break;
                     self.publish({ { "{index}", String(i) } }, String(C), "°C");
                 }
-            }
-        });
+            },
+        };
+        snuffels.push_back(s);
     }
 
     {
         static HardwareSerial hwserial(2);
         static MHZ19 mhz;
 
-        snuffels.push_back(SnuffelSensor {
-            "MH-Z19",
-            "CO2 sensor",
-            "co2",
-            []() { // init
+        struct SnuffelSensor s = {
+            enabled: false,  // enabled by default via WiFiSettings
+            id: "MH-Z19",
+            description: "CO2 sensor",
+            topic_suffix: "co2",
+            init: []() {
                 hwserial.begin(9600, SERIAL_8N1, 22, 21);
                 mhz.begin(hwserial);
                 mhz.autoCalibration();
             },
-            []() { // request
+            request: []() {
                 // empty
             },
-            [](SnuffelSensor& self) { // fetch
+            fetch: [](SnuffelSensor& self) {
                 int CO2 = mhz.getCO2();
                 self.publish(String(CO2), "PPM");
             }
-        });
+        };
+        snuffels.push_back(s);
     }
 
     {
@@ -112,24 +108,26 @@ void setup_sensors() {
         static PMS pms(hwserial);
         static PMS::DATA data;
 
-        snuffels.push_back(SnuffelSensor {
-            "PMS7003",
-            "dust sensor",
-            "dust/PM{size}",
-            []() { // init
+        struct SnuffelSensor s = {
+            enabled: false,  // enabled by default via WiFiSettings
+            id: "PMS7003",
+            description: "dust sensor",
+            topic_suffix: "dust/PM{size}",
+            init: []() {
                 hwserial.begin(9600, SERIAL_8N1, 25, 32);
                 pms.passiveMode();
             },
-            []() { // request
+            request: []() {
                 pms.requestRead();
             },
-            [](SnuffelSensor& self) { // fetch
+            fetch: [](SnuffelSensor& self) {
                 if (! pms.readUntil(data)) return;
                 self.publish({ { "{size}",  "1.0" } }, String(data.PM_AE_UG_1_0),  "µg/m3");
                 self.publish({ { "{size}",  "2.5" } }, String(data.PM_AE_UG_2_5),  "µg/m3");
                 self.publish({ { "{size}", "10.0" } }, String(data.PM_AE_UG_10_0), "µg/m3");
             }
-        });
+        };
+        snuffels.push_back(s);
     }
 }
 
@@ -149,7 +147,8 @@ void setup() {
 
     String server = WiFiSettings.string("mqtt_server", 64, "test.mosquitto.org", "MQTT broker");
     int port      = WiFiSettings.integer("mqtt_port", 0, 65535, 1883, "MQTT broker TCP port");
-    topic_prefix  = WiFiSettings.string("mqtt_prefix", "snuffelaar/", "MQTT topic prefix (ending with '/' strongly advised)");
+    topic_prefix  = WiFiSettings.string("snuffelaar_mqtt_prefix", "snuffelaar/", "MQTT topic prefix (ending with '/' strongly advised)");
+    add_units     = WiFiSettings.checkbox("snuffelaar_add_units", true, "Add units of measurement to MQTT messages");
 
     for (auto& s : snuffels) {
         String label = "Enable " + s.id + " " + s.description;
